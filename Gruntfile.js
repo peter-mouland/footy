457772350e8309fcfde833538bcc79b550cfb63e
Peter Mouland <peter.mouland@gmail.com>
2014-03-20 00:46:51 +0000

    move to node js

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,21 +4,13 @@ module.exports = function (grunt) {
   // Load all grunt tasks
   require('load-grunt-tasks')(grunt);
 
-    var dateFormat = require('dateformat');
-    var hour = dateFormat(new Date(), "HH");
-    var folder = dateFormat(new Date(), "yyyy-mm-dd-ddd") + '/';
-
-
-    if (parseInt(hour,10) < 19){
-        console.error('please wait until after 7pm!!');
-        return;
-    }
-
   // Project configuration.
   grunt.initConfig({
+
     nodeunit: {
       files: ['test/**/*_test.js']
     },
+
     jshint: {
       options: {
         jshintrc: '.jshintrc',
@@ -37,76 +29,7 @@ module.exports = function (grunt) {
     exec: {
 
     },
-    curl: {
-        stats: {
-            src: [{
-                url: 'https://fantasyfootball.skysports.com/statistics/',
-                method: 'GET'
-            }],
-            dest: './stats/' + folder + 'stats.html'
-        }
-    },
-    dom_munger: {
-      read: {
-          options: {
-              read: [
-                  {selector:'.STFFDataTable', writeto:'stats', isPath:false} //<%= dom_munger.data.stats %>
-              ],
-              callback: function($, fileName){
-                  var o = {  mapHeadings:{}, arrHeadings:[], arrStats: [], mapStats : {} };
-                  var $th = $('.STFFDataTable th');
-                  var $tr = $('.STFFDataTable tr:not(:first-child)');
-                  var i, th, els, el, nodes, node, td, player;
-                  for (th in $th){
-                      if ($th.hasOwnProperty(th)) {
-                          el = $th[th];
-                          if (th !== 'length'){
-                              o.arrHeadings.push(el.children[0].data);
-                              o.mapHeadings[el.children[0].data] = el.attribs['title'];
-                          }
-                      }
-                  }
-                  for (nodes in $tr){
-                      player = {};
-                      i = 0;
-                      if ($tr.hasOwnProperty(nodes)) {
-                          node = $tr[nodes];
-                          if (nodes !== 'length'){
-                              for (els in node.children){
-                                  if (node.children.hasOwnProperty(els) ) {
-                                      el = node.children[els];
-                                      if (el.name == 'td' && o.arrHeadings[i].trim){
-                                          i++;
-                                          player[o.arrHeadings[i]] = el.children[0].data;
-                                      }
-                                  }
-                              }
-                          }
-                      }
-                      o.arrStats.push(player);
-                  }
-                  o.arrStats.forEach(function(stats, i){o.mapStats[stats.Name] = stats});
-                  $('html').html(JSON.stringify(o));
-              }
-          },
-          src: 'stats/' + folder + 'stats.html', //could be an array of files
-          dest: 'stats/' + folder + 'data.html' //optional, if not specified the src file will be overwritten
-      },
-    },
-    copy:{
-        stats:{
-            options: {
-                process: function (content, srcpath) {
-                    var newContent = content.replace('<!doctype html>',"");
-                    newContent = newContent.replace('<html lang="en">',"");
-                    newContent = newContent.replace('</html>',"");
-                    return newContent
-                }
-            },
-            src: './stats/' + folder + 'data.html',
-            dest: './stats/' + folder + 'data.json'
-        }
-    },
+
     watch: {
       gruntfile: {
         files: '<%= jshint.gruntfile.src %>',
@@ -123,7 +46,7 @@ module.exports = function (grunt) {
     }
   });
 
-  // Default task.
-  grunt.registerTask('default', ['curl', 'dom_munger', 'copy']);
+    // Default task.
+  grunt.registerTask('default', ['jshint']);
 
 };
--- app.js
+++ app.js
@@ -10,9 +10,99 @@
 
     var express = require('express');
     var app = express();
+    app.root = __dirname + '/';
+
+    var fs = require('fs');
+    var request = require('request');
+    var cheerio = require('cheerio');
+
+    function getStats($,addForEach){
+        var o = {  mapHeadings:{}, arrHeadings:[], arrStats: [], mapStats : {} };
+        var $th = $('.STFFDataTable th');
+        var $tr = $('.STFFDataTable tr:not(:first-child)');
+        var i, th, els, el, nodes, node, td, player;
+        for (th in $th){
+            if ($th.hasOwnProperty(th)) {
+                el = $th[th];
+                if (th !== 'length' && el.children[0]){
+                    o.arrHeadings.push(el.children[0].data);
+                    o.mapHeadings[el.children[0].data] = el.attribs['title'];
+                }
+            }
+        }
+        for (nodes in $tr){
+            player = {};
+            i = 0;
+            if ($tr.hasOwnProperty(nodes)) {
+                node = $tr[nodes];
+                if (nodes !== 'length'){
+                    for (els in node.children){
+                        if (node.children.hasOwnProperty(els) ) {
+                            el = node.children[els];
+                            if (el.name == 'td' && o.arrHeadings[i].trim){
+                                player[o.arrHeadings[i]] = el.children[0].data;
+                                i++;
+                            }
+                        }
+                    }
+                }
+            }
+            o.arrStats.push(player);
+        }
+        o.arrStats.forEach(function(stats, i){o.mapStats[stats.Name] = stats});
+        return o;
+    }
 
     app.get('/', function(req, res){
-        res.send('Hello World');
+        res.send('hello world');
+    });
+
+    app.get('/info', function(req, res){
+        var week = require(app.root + 'stats/current-week.json');
+        res.send('current week: ' + week.CURRENTWEEK + '. week complete: ' + week.SUCCESS);
+    });
+
+    app.get('/update', function(req, res){
+        var week = require(app.root + 'stats/current-week.json');
+        request('https://fantasyfootball.skysports.com/json/teaminfo/0', function(err, response, body){
+            var newJson = JSON.parse(body);
+            var newWeek = newJson.CURRENTWEEK;
+            var isUpToDate = week.CURRENTWEEK == newWeek;
+            if (isUpToDate){
+                res.send('nothing to do');
+            } else {
+                week = newJson;
+                var outputFilename = app.root + 'stats/current-week.json';
+                fs.writeFile(outputFilename, JSON.stringify(newJson, null, 2), function(err) {
+                    if(err) {
+                        console.log(err);
+                    } else {
+                        console.log("JSON saved to " + outputFilename);
+                        request('https://fantasyfootball.skysports.com/statistics/', function(err, response, body) {
+                            var $ = cheerio.load(body);
+                            var stats = getStats($, Array.prototype.slice);
+                            var outputFilename = app.root + 'stats/' + newWeek + '/stats.json';
+                            fs.writeFile(outputFilename, JSON.stringify(stats, null, 2), function(err) {
+                                if(err) {
+                                    console.log(err);
+                                } else {
+                                    console.log("JSON saved to " + outputFilename);
+                                    res.send(stats);
+                                }
+                            });
+                        });
+                    }
+                });
+            }
+        });
+    });
+
+    app.get('/compare', function(req, res){
+        var week = require(app.root + 'stats/current-week.json').CURRENTWEEK;
+        var prevWeek = parseInt(week,10) - 1;
+        var cur = require(app.root + 'stats/' + week + '/stats.json');
+        var prev = require(app.root + 'stats/' + prevWeek + '/stats.json');
+        res.send(prev.arrStats[0].Total + ' | ' + cur.arrStats[0].Total );
     });
 
     var server = app.listen(3000, function() {
